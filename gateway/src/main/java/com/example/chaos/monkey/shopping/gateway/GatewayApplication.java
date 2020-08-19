b3093303bb14e0d6e92760574b48d48602a2ca75
Ryan Baxter <rbaxter@vmware.com>
2020-08-19 12:27:05 -0400

    Added some demo CB customizations

--- gateway/src/main/java/com/example/chaos/monkey/shopping/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/chaos/monkey/shopping/gateway/GatewayApplication.java
@@ -1,15 +1,25 @@
 package com.example.chaos.monkey.shopping.gateway;
 
+import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 
 import com.example.chaos.monkey.shopping.domain.Product;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.loadbalancer.core.DiscoveryClientServiceInstanceListSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +30,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableDiscoveryClient
 public class GatewayApplication {
+
+	private static final Log LOG = LogFactory
+			.getLog(GatewayApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
@@ -43,6 +56,32 @@ public class GatewayApplication {
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("fallback", "true");
 		return ResponseEntity.ok().headers(headers).body(Collections.emptyList());
+	}
+
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> toysCustomizer() {
+		return factory -> {
+			factory.configure(builder -> {
+				builder
+						.timeLimiterConfig(TimeLimiterConfig.ofDefaults())
+						.circuitBreakerConfig(CircuitBreakerConfig.custom()
+								.failureRateThreshold(1) //If the failure rate is greater than 1% open the CB
+								.minimumNumberOfCalls(1) //Only 1 call needs to be made before the failure rate threshold is computer
+								.waitDurationInOpenState(Duration.ofSeconds(5)) //Wait five seconds before transitioning the CB from open to half opened to allow calls to go through
+								.build());
+
+			}, "toys");
+			factory.addCircuitBreakerCustomizer(
+					circuitBreaker -> circuitBreaker.getEventPublisher()
+							.onError(event -> {
+								LOG.warn(circuitBreaker.getMetrics().getFailureRate());
+								LOG.warn(circuitBreaker.getMetrics().getNumberOfFailedCalls());
+								LOG.warn("Toys circuit breaker had an error " + circuitBreaker.getState(), event.getThrowable());
+							})
+							.onSuccess(event -> {
+								LOG.info("No error here");
+							}), "toys");
+		};
 	}
 
 }
