00a320c31f92af26dbf1712993058b526e4295dc
Ryan Baxter <rbaxter@vmware.com>
2020-08-21 08:50:09 -0400

    Added filter for the gateway

--- gateway/src/main/java/com/example/chaos/monkey/shopping/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/chaos/monkey/shopping/gateway/GatewayApplication.java
@@ -1,30 +1,43 @@
 package com.example.chaos.monkey.shopping.gateway;
 
 import java.time.Duration;
+import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
 
 import com.example.chaos.monkey.shopping.domain.Product;
+import com.example.chaos.monkey.shopping.domain.ProductCategory;
+import com.example.chaos.monkey.shopping.gateway.domain.ProductResponse;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Mono;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreaker;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
-import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.rewrite.MessageBodyDecoder;
+import org.springframework.cloud.gateway.filter.factory.rewrite.MessageBodyEncoder;
+import org.springframework.cloud.gateway.filter.factory.rewrite.ModifyResponseBodyGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.rewrite.RewriteFunction;
+import org.springframework.cloud.gateway.handler.predicate.BetweenRoutePredicateFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.loadbalancer.core.DiscoveryClientServiceInstanceListSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.HttpMessageReader;
+import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 
 @SpringBootApplication
 @RestController
@@ -39,10 +52,18 @@ public class GatewayApplication {
 	}
 
 	@Bean
-	public RouteLocator routes(RouteLocatorBuilder builder) {
+	public AddProductFilterFactory addProductFilterFactory(ServerCodecConfigurer codecConfigurer, Set<MessageBodyDecoder> bodyDecoders,
+			Set<MessageBodyEncoder> bodyEncoders, BetweenRoutePredicateFactory betweenRoutePredicateFactory) {
+		return new AddProductFilterFactory(codecConfigurer.getReaders(), bodyDecoders, bodyEncoders, betweenRoutePredicateFactory);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder, AddProductFilterFactory addProductFilterFactory) {
+		GatewayFilter addProductFilter = addProductFilterFactory.apply();
 		return builder.routes()
 				.route(p -> p.path("/hotdeals**").filters(f ->
-						f.circuitBreaker(c -> c.setName("hostdeals").setFallbackUri("forward:/fallback"))).uri("lb://hotdeals"))
+						f.circuitBreaker(c -> c.setName("hostdeals").setFallbackUri("forward:/fallback"))
+								.filter(addProductFilter)).uri("lb://hotdeals"))
 				.route(p -> p.path("/fashion/**").filters(f ->
 						f.circuitBreaker(c -> c.setName("fashion").setFallbackUri("forward:/fallback"))).uri("lb://fashion-bestseller"))
 				.route(p -> p.path("/toys/**").filters(f ->
@@ -57,6 +78,7 @@ public class GatewayApplication {
 		headers.add("fallback", "true");
 		return ResponseEntity.ok().headers(headers).body(Collections.emptyList());
 	}
+
 
 	@Bean
 	public Customizer<ReactiveResilience4JCircuitBreakerFactory> toysCustomizer() {
@@ -84,4 +106,45 @@ public class GatewayApplication {
 		};
 	}
 
+	class AddProductFilterFactory extends ModifyResponseBodyGatewayFilterFactory {
+		private BetweenRoutePredicateFactory betweenRoutePredicateFactory;
+		AddProductFilterFactory(List<HttpMessageReader<?>> messageReaders, Set<MessageBodyDecoder> messageBodyDecoders, Set<MessageBodyEncoder> messageBodyEncoders, BetweenRoutePredicateFactory betweenRoutePredicateFactory) {
+			super(messageReaders, messageBodyDecoders, messageBodyEncoders);
+			this.betweenRoutePredicateFactory = betweenRoutePredicateFactory;
+		}
+
+
+		public GatewayFilter apply() {
+			Config config = new Config();
+			config.setInClass(List.class);
+			config.setOutClass(List.class);
+			config.setRewriteFunction(new AddProductRewriteFunction(betweenRoutePredicateFactory));
+			return apply(config);
+		}
+	}
+
+	class AddProductRewriteFunction implements RewriteFunction<List, List> {
+
+		private Predicate<ServerWebExchange> between;
+
+		public AddProductRewriteFunction(BetweenRoutePredicateFactory betweenRoutePredicateFactory) {
+			ZonedDateTime now = ZonedDateTime.now();
+			this.between = betweenRoutePredicateFactory.apply(new BetweenRoutePredicateFactory.Config().setDatetime1(now).setDatetime2(now.plusMinutes(1)));
+		}
+
+		@Override
+		public Publisher<List> apply(ServerWebExchange exchange, List products) {
+			if(between.test(exchange)) {
+				ProductResponse modified = new ProductResponse();
+				Product secret = new Product();
+				secret.setId(123);
+				secret.setCategory(ProductCategory.TOYS);
+				secret.setName("XBOX");
+				products.add(secret);
+				return Mono.just(products);
+			}
+			return Mono.just(products);
+		}
+	}
 }
+
