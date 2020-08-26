8ec044c4b005a5f11ccc9ee38212ffaccb530b78
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2020-08-26 18:07:06 +0200

    Add code for LoadBalancer demo.

--- shop/src/main/java/com/example/chaos/monkey/shopping/shop/ShopApplication.java
+++ shop/src/main/java/com/example/chaos/monkey/shopping/shop/ShopApplication.java
@@ -1,5 +1,6 @@
 package com.example.chaos.monkey.shopping.shop;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
+//@LoadBalancerClient(name = "fashion-bestseller", configuration = CustomLoadBalancerConfiguration.class)
 public class ShopApplication {
 
 	public static void main(String[] args) {
