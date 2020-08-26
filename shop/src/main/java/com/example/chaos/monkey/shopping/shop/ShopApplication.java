68a3ef94c1a876a7ea664fe2f7ad66d4d64ccaf4
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2020-08-26 14:40:26 +0200

    Add shop service.

---
+++ shop/src/main/java/com/example/chaos/monkey/shopping/shop/ShopApplication.java
@@ -0,0 +1,15 @@
+package com.example.chaos.monkey.shopping.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Olga Maciaszek-Sharma
+ */
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+}
